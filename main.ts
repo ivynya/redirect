065f54052668b8bbf3459714fbb073a48d82d37d
SDBagel <sdbagel@gmail.com>
2021-12-22 02:03:27 -0800

    Validate redirects with Notion

--- main.ts
+++ main.ts
@@ -6,10 +6,24 @@ const app = new Application();
 
 app.use(async ctx => {
 	const name = ctx.request.url.pathname;
-	console.log(`[LOG] Parsed ${name}`);
-	console.log(await queryDatabase());
-	ctx.response.status = 302;
-	ctx.response.headers.set("Location", `https://sdbagel.com${name}`);
+
+	const validRedirects = await queryDatabase();
+	const redir = validRedirects.find(r =>
+		r.Short[0].content === name || r.Short[0].content === name.slice(1));
+
+	if (redir) {
+		ctx.response.status = 302;
+		ctx.response.headers.set("Location", redir.RedirectURL.url);
+		console.log(`[LOG] Parsed ${name} > ${redir.RedirectURL.url}`);
+	} else {
+		ctx.response.status = 400;
+		ctx.response.body = "The redirect you requested does not exist.";
+		console.log(`[LOG] Invalid ${name} > 404`);
+	}
+
+	if (redir.CampaignID) {
+		// Make a request to the companion Analytics API
+	}
 });
 
 console.log("[EVT] Listening on http://localhost:8000");
