ed880c0a9314ed613ea5f13c963b16bbb4ef04dd
SDBagel <sdbagel@gmail.com>
2021-12-23 00:57:51 -0800

    Use notion_flatten@v0.2.1, make analytics requests

--- deps.ts
+++ deps.ts
@@ -2,4 +2,4 @@
 import "https://deno.land/x/dotenv@v3.1.0/load.ts";
 
 export { Application } from "https://deno.land/x/oak@v10.1.0/mod.ts";
-export { flattenResult } from "https://deno.land/x/notion_flatten@v0.1.0/mod.ts"
+export { flattenQuery } from "https://deno.land/x/notion_flatten@v0.2.1/mod.ts"
--- main.ts
+++ main.ts
@@ -9,20 +9,25 @@ app.use(async ctx => {
 
 	const validRedirects = await queryDatabase();
 	const redir = validRedirects.find(r =>
-		r.Short[0].content === name || r.Short[0].content === name.slice(1));
+		r.Short === name || r.Short === name.slice(1));
 
 	if (redir) {
 		ctx.response.status = 302;
-		ctx.response.headers.set("Location", redir.RedirectURL.url);
-		console.log(`[LOG] Parsed ${name} > ${redir.RedirectURL.url}`);
+		ctx.response.headers.set("Location", redir.RedirectURL);
+		console.log(`[LOG] Parsed ${name} > ${redir.RedirectURL}`);
 	} else {
 		ctx.response.status = 400;
 		ctx.response.body = "The redirect you requested does not exist.";
 		console.log(`[LOG] Invalid ${name} > 404`);
+		return;
 	}
 
 	if (redir.CampaignID) {
 		// Make a request to the companion Analytics API
+		const apiBase = Deno.env.get("ANALYTICS_API_HOST");
+		await fetch(`https://${apiBase}/v1/campaign/${redir.CampaignID}`,
+			{ method: "POST" });
+		console.log(`[LOG] Analytics request for ${redir.RedirectURL}:${redir.CampaignID}`);
 	}
 });
 
