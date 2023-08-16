17890c77ade454120ee57cd28fc1b87a16cd5e48
ivynya <ivy@ivy.direct>
2023-08-16 13:24:36 -0700

    fix: incorrect env reporting on init

--- cmd/redirect/main.go
+++ cmd/redirect/main.go
@@ -17,11 +17,11 @@ func main() {
 
 	createRouter(app)
 
-	analyticsPresent := os.Getenv("ANALYTICS_API_HOST") != ""
 	tokenPresent := os.Getenv("NOTION_TOKEN") != ""
 	log.Println("[redirect] $NOTION_DB_ID:", os.Getenv("NOTION_DB_ID"))
 	log.Println("[redirect] $NOTION_TOKEN:", tokenPresent)
-	log.Println("[redirect] $ANALYTICS_API_HOST:", analyticsPresent)
+	log.Println("[redirect] $ANALYTICS_HOST:", os.Getenv("ANALYTICS_HOST"))
+	log.Println("[redirect] $ANALYTICS_VERSION:", os.Getenv("ANALYTICS_VERSION"))
 	log.Println("[redirect] starting fiber on port 3000")
 
 	log.Fatal(app.Listen(":3000"))
